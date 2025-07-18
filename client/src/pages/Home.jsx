bb7e023b5e5ca7e1b5850bad7483cc6081fe762d
smRid <omorsk911@gmail.com>
2025-07-18 06:27:55 +0600

    Add AiTools component to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import AiTools from "../components/AiTools"
 import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
 
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Navbar />
       <Hero /> 
+      <AiTools />
     </>
   )
 }
